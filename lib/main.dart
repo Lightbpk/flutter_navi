be7e0c533ea38e6ad105842e1e335f7d0ddc581d
Lightbpk <bpk735@gmail.com>
2021-03-05 01:26:11 +0700

    'retun_value'

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,20 @@ import 'package:flutter/material.dart';
 class MainScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    final _scaffoldKey = GlobalKey<ScaffoldState>();
       return Scaffold(
+        key: _scaffoldKey,
         appBar: AppBar(title: Text("Вопрос")),
         body: Center(child: Column(children: [
-          RaisedButton(onPressed: (){
-            Navigator.push(context, PageRouteBuilder(
+          RaisedButton(onPressed: () async{
+            bool valueDialog = await Navigator.push(context, PageRouteBuilder(
                 opaque: false, pageBuilder: (BuildContext context,_,__) => MyPopup(),
             transitionsBuilder: (___, Animation<double> animation,____, Widget child){
                   return FadeTransition(opacity:animation,
-                  child: ScaleTransition(scale: animation,child: childgitgit,));
+                  child: ScaleTransition(scale: animation,child: child,));
             }));
+            if(valueDialog) _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text('Yes'),backgroundColor: Colors.green,));
+            else _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text('No'),backgroundColor: Colors.red,));
           },child: new Text("Вопрос"),)
         ],),),
       );
@@ -26,8 +30,8 @@ class MyPopup extends StatelessWidget {
   Widget build(BuildContext context) {
     return AlertDialog(
       title: new Text('Ответ'),
-      actions: [FlatButton(onPressed: (){Navigator.pop(context);}, child: Text('Да')),
-      FlatButton(onPressed: (){Navigator.pop(context);}, child: Text('нет'))],
+      actions: [FlatButton(onPressed: (){Navigator.pop(context,true);}, child: Text('Да')), // dialog value var  true
+      FlatButton(onPressed: (){Navigator.pop(context,false);}, child: Text('нет'))],// false
     );
   }
 
